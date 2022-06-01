b11904e2f1b53ab79f533a8b35891851773d67b6
Gerardo Ayes <gerardoayes92@gmail.com>
2022-06-01 15:17:30 -0400

    database and settings setup

--- questionbox/settings.py
+++ questionbox/settings.py
@@ -25,8 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+environ.Env.read_env()
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
