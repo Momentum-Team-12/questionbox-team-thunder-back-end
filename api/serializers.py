6e19fd39691380a9adb5ee9f8f2c3e074daafb34
Manny Dador <EADador@gmail.com>
2022-06-01 17:09:12 -0400

    AnswerSerializer to serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,3 +1,16 @@
+from pyexpat import model
 from rest_framework import serializers
 from .models import User, Question, Answer, Favorite
 
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model: Answer
+        fields = (
+            'pk',
+            'author',
+            'description',
+            'created_at',
+            'question',
+            'favorite',
+            'accepted',
+        )
