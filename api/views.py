1cb6df180da47629dcd6c1c47a5028d3cfc74c09
Manny Dador <EADador@gmail.com>
2022-06-01 17:55:28 -0400

    AnswerDetail and api_root serializer

--- api/views.py
+++ api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.reverse import reverse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import generics
+from .models import User, Question, Answer, Favorite
+from serializers import AnswerSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('api-user-list', request=request, format=format),
+        'questions': reverse('api-question-list', request=request, format=format),
+        'records': reverse('api-answer-list', request=request, format=format),
+    })
+
+class AnswerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+
+    def perform_destroy(self, instance):
+        instance.delete()
