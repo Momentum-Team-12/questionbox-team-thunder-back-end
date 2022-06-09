640d587612b3fb632e07c34fe885f01a5b7e2a65
Gerardo Ayes <gerardoayes92@gmail.com>
2022-06-09 16:46:09 -0400

    minor changes

--- api/views.py
+++ api/views.py
@@ -2,8 +2,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.reverse import reverse
 from django.shortcuts import get_object_or_404
-
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from django.db.models import Count
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import User, Question, Answer
 from .serializers import (
     UserSerializer,
@@ -21,13 +21,7 @@ from .serializers import (
 from rest_framework import viewsets
 from rest_framework.generics import ListAPIView, UpdateAPIView
 from django.db.models.query import QuerySet
-from .custom_permissions import (
-    ReadOnly,
-    )
-from rest_framework import generics
-
 from django.db.models import Q
-from rest_framework import filters
 
 
 class UserViewSet(viewsets.ViewSet):
@@ -62,18 +56,14 @@ class QuestionViewSet(viewsets.ModelViewSet):
         return super().get_serializer_class()
 
     def get_queryset(self):
-        assert self.queryset is not None, (
-            "'%s' should either include a `queryset` attribute, "
-            "or override the `get_queryset()` method."
-            % self.__class__.__name__
-        )
-
         queryset = self.queryset
         if isinstance(queryset, QuerySet):
             queryset = queryset.all()
             if self.request.user.is_authenticated:
                 queryset = queryset.filter(author=self.request.user)
-        return queryset
+        return queryset.annotate(
+                total_answers=Count('answers')
+            )
 
     def perform_create(self, serializer):
         if self.request.user.is_authenticated:
@@ -128,12 +118,6 @@ class AnswerListRetrieve(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def get_queryset(self):
-        assert self.queryset is not None, (
-            "'%s' should either include a `queryset` attribute, "
-            "or override the `get_queryset()` method."
-            % self.__class__.__name__
-        )
-
         queryset = self.queryset
         if isinstance(queryset, QuerySet):
             queryset = queryset.all()
@@ -158,26 +142,29 @@ class AllQuestionView(viewsets.ViewSet):
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def get_queryset(self):
-            search_term = self.request.query_params.get("search")
-            if search_term is not None:
-                results = Question.objects.filter(
-                    Q(title__icontains=search_term) | 
-                    Q(description__icontains=search_term))
+        search_term = self.request.query_params.get("search")
+        if search_term is not None:
+            results = Question.objects.filter(
+                Q(title__icontains=search_term) |
+                Q(description__icontains=search_term)
+            )
+            results.order_by('-id')
 
-            else:
-                results = self.queryset.all()
-            return results.order_by('-id')
+        else:
+            results = self.queryset.all()
+            results.order_by('-id')
+        return results.annotate(
+            total_answers=Count('answers')
+        )
 
     def list(self, request):
         queryset = self.get_queryset()
         serializer = QuestionListSerializer(queryset, many=True)
-
         return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
         queryset = self.get_queryset()
         question = get_object_or_404(queryset, pk=pk)
-
         serializer = QuestionRetrieveSerializer(question)
         return Response(serializer.data)
 
@@ -244,7 +231,7 @@ class AllAnswerViewSet(viewsets.ModelViewSet):
             serializer.save(author=self.request.user, question=question)
 
     def perform_update(self, serializer):
-        if self.request.user == serializer.instance.author:
+        if self.request.user == serializer.instance.question.author:
             serializer.save()
 
     def perform_destroy(self, instance):
